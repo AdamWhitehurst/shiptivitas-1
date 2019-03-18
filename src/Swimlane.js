610675433a3d636656a8d9dcbcc1427dc371a3cd
AdamWhitehurst <Adam.Whitehurst@live.com>
2019-03-18 08:52:12 -0700

    Convert to functional components

--- src/Swimlane.js
+++ src/Swimlane.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import Card from './Card';
-import './Swimlane.css';
+import React from "react";
+import Card from "./Card";
+import "./Swimlane.css";
 
-export default class Swimlane extends React.Component {
-  render() {
-    const cards = this.props.clients.map(client => {
-      return (
-        <Card
-          key={client.id}
-          id={client.id}
-          name={client.name}
-          description={client.description}
-          status={client.status}
-        />
-      );
-    })
+// A simple presentational component demonstrating
+// the functional component pattern
+export default ({ clients, name, dragulaRef }) => {
+  const cards = clients.map(client => {
     return (
-      <div className="Swimlane-column">
-        <div className="Swimlane-title">{this.props.name}</div>
-        <div className="Swimlane-dragColumn" ref={this.props.dragulaRef}>
-          {cards}
-        </div>
-      </div>);
-  }
+      <Card
+        key={client.id}
+        id={client.id}
+        name={client.name}
+        description={client.description}
+        status={client.status}
+      />
+    );
+  });
 
-}
+  return (
+    <div className="Swimlane-column">
+      <div className="Swimlane-title">{name}</div>
+      <div className="Swimlane-dragColumn" ref={dragulaRef}>
+        {cards}
+      </div>
+    </div>
+  );
+};
